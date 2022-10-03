c53a516794e8a89d769b85c4479a2d75006304bc
eloygplaza <10582213+eloygplaza@users.noreply.github.com>
2022-10-03 18:53:10 +0200

    recent projects done with JSON

--- js/index.js
+++ js/index.js
@@ -10,26 +10,50 @@ async function fetchProjects() {
 
   // iteramos por todos los projects
   projects.forEach((element) => {
-    let titleCard = element.project - title; // titulo card
-    let pathImageCard = `./projects-assets/projects-section/${
-      element.project - card - id
-    }.jpg`; // url de la imagen
-    let projectDescription = element.project - description; // descripcion card
+    let titleCard = element.title; // titulo card
+    let pathImageCard = `./project-assets/projects-section/${element.id}.jpg`; // url de la imagen
+    let projectDescription = element.description; // descripcion card
 
     // creamos lo elementos a insertar en el html
-    const block = document.createElement("a");
-    block.classList.add("panel-block");
-    const blockSpan = document.createElement("span");
-    const spanImg = document.createElement("img");
-    spanImg.src = imagePath;
-    spanImg.alt = element.name;
-    const spanText = document.createTextNode(`${element.name}`);
+    const divItem = document.createElement("div");
+    divItem.classList.add("projects-item");
+    const divCard = document.createElement("div");
+    divCard.classList.add("projects-card");
+    const divCardImage = document.createElement("div");
+    divCardImage.classList.add("projects-image");
+    const imgCard = document.createElement("img");
+    imgCard.src = pathImageCard;
+    imgCard.classList.add("projects-card-image");
+    const divCardContent = document.createElement("div");
+    divCardContent.classList.add("projects-content");
+    const divCardText = document.createElement("div");
+    divCardText.classList.add("projects-text");
+    const divCardTitle = document.createElement("div");
+    divCardTitle.classList.add("projects-title");
+    divCardTitle.innerText = titleCard;
+    const divCardDescription = document.createElement("div");
+    divCardDescription.classList.add("projects-title");
+    divCardDescription.classList.add("projects-description");
+    divCardDescription.innerText = projectDescription;
+    const linkProject = document.createElement("a");
+    linkProject.classList.add("projects-link");
+    linkProject.innerText = "Learn More";
 
     // anexamos e insertamos en html
-    blockSpan.appendChild(spanImg);
-    block.appendChild(blockSpan);
-    block.appendChild(spanText);
-    container.appendChild(block);
+    divCardText.appendChild(divCardTitle);
+    divCardText.appendChild(divCardDescription);
+    divCardText.appendChild(linkProject);
+    divCardContent.appendChild(divCardText);
+    divCardImage.appendChild(imgCard);
+    divCard.appendChild(divCardImage);
+    divCard.appendChild(divCardContent);
+    divItem.appendChild(divCard);
+    container.appendChild(divItem);
+
+    // document.querySelector(".projects-title").innerText = titleCard;
+    // document.querySelector(".projects-description").innerText =
+    //   projectDescription;
+    // document.querySelector(".projects-link").innerText = "Learn More";
   });
 }
 
